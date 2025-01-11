d287510f6ef3e34923594cbf3c749b979c4a433f
endafigueroa <endafigueroa@gmail.com>
2025-01-11 17:09:26 -0500

    please

---
+++ streamlit_cadence.py
@@ -0,0 +1,145 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+
+#######################  Page Config
+st.set_page_config(
+    page_title="ZipSpontify User Demographic Analytics over 12 Weeks",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+#######################
+# CSS styling
+st.markdown("""
+<style>
+
+[data-testid="block-container"] {
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 1rem;
+    padding-bottom: 0rem;
+    margin-bottom: -7rem;
+}
+
+[data-testid="stVerticalBlock"] {
+    padding-left: 0rem;
+    padding-right: 0rem;
+}
+
+[data-testid="stMetric"] {
+    background-color: #393939;
+    text-align: center;
+    padding: 15px 0;
+}
+
+[data-testid="stMetricLabel"] {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+[data-testid="stMetricDeltaIcon-Up"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+[data-testid="stMetricDeltaIcon-Down"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+
+#######################
+# Load data
+omega_raw = pd.read_json('data/omega_raw.json', lines=True)
+est_df = pd.read_json('data/est_df.json', lines=True)
+cst_df = pd.read_json('data/cst_df.json', lines=True)
+mst_df = pd.read_json('data/mst_df.json', lines=True)
+pst_df = pd.read_json('data/pst_df.json', lines=True)
+hst_df = pd.read_json('data/hst_df.json', lines=True)
+
+
+#######################
+# Sidebar
+
+with st.sidebar:
+    st.header('Time Zone')
+    sb_tz = st.multiselect("Select one or more options:",
+        ['EST','CST','MST','PST','HST'], key='option')
+    
+    all_options = st.button("Select all options")
+    
+    if all_options:
+        sb_tz = ['EST','CST','MST','PST','HST']
+
+    st.header('Week')
+    sb_wk = st.multiselect("Week",['Week 1','Week 2', 'Week 3','Week 4','Week 5'], key='option_2')
+    sb_wk = sb_wk.append('Present')
+    all_options_w = st.button("Select all Weeks")
+
+    if all_options_w:
+        sb_wk = ['Week 1', 'Week 2', 'Week 3','Week 4','Week 5','Present']
+    
+################ Begin DF corelation to feed into Dashboard
+    df_seled_wk =pd.DataFrame(columns=['artist', 'song','duration','ts','sessionId','level','state','userAgent','userId','firstName','gender','week','time_zone'])
+
+    for i in sb_tz:
+        select_tz= omega_raw.loc[omega_raw['time_zone'] == i]
+        df_seled_wk=pd.concat([df_seled_wk,select_tz])
+    df_selected_week= df_seled_wk[df_seled_wk['week'].isin(sb_wk)]
+
+    # color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
+    # selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+
+
+#######################
+# Plots
+######## Gender
+
+####### State User Counts
+
+####### Artist
+
+####### Song
+
+####### Platform
+
+def most_used_platform(df_selected_week):
+    platform=df_selected_week.groupby('userId')['userAgent'].first().value_counts()
+    ##Donut Chart
+    fig = px.pie(platform, names=platform.index, values=platform, hole=.6)
+    fig.update_traces(textinfo='percent+label')
+    # fig.update_layout(title_text=f"Most Used Platforms in {df_selected_tz.iloc[1,12]} in {selected_week}")
+    return fig
+
+####### Level
+
+####### Duration
+
+#######################
+# Dashboard Main Panel
+
+
+st.markdown('#### Platforms')
+donut = most_used_platform(df_selected_week)
+st.plotly_chart(donut, use_container_width=True)
+    
+    
+with st.expander('About', expanded=True):
+    st.write('''
+            - Data: [U.S. Census Bureau](https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html).
+            - :orange[**Gains/Losses**]: states with high inbound/ outbound migration for selected year
+            - :orange[**States Migration**]: percentage of states with annual inbound/ outbound migration > 50,000
+            ''')
